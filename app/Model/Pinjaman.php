6a5088bcf1274b7ac572ac65230baaba71358de0
charlyhs24 <charlysitumorang24@gmail.com>
2018-11-06 18:50:49 +0700

    123

--- app/Model/Pinjaman.php
+++ app/Model/Pinjaman.php
@@ -8,7 +8,7 @@ class Pinjaman extends Model
 {
     //
     protected $table = 'pinjaman';
-    protected $primaryKey = 'id_pinjaman'; 
+    protected $primarykey = 'id_pinjaman'; 
     
     protected $fillable = [
         'nim_peminjam','nama_peminjam','asal_organisasi', 'tanggal_peminjaman', 'status', 'url_surat', 'url_ktm', 'status_pengajuan',
