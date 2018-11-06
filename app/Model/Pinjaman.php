9fc647b0d21d2187bcb801b6be4e01a5765f7824
charlyhs24 <charlysitumorang24@gmail.com>
2018-11-06 20:40:42 +0700

    add primary

--- app/Model/Pinjaman.php
+++ app/Model/Pinjaman.php
@@ -9,8 +9,5 @@ class Pinjaman extends Model
     //
     protected $table = 'pinjaman';
     protected $primaryKey = 'id_pinjaman'; 
-    
-    protected $fillable = [
-        'nim_peminjam','nama_peminjam','asal_organisasi', 'tanggal_peminjaman', 'status', 'url_surat', 'url_ktm', 'status_pengajuan',
-    ];
+    protected $incrementing = true;
 }
